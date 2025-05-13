20f7a29857b4cdaad062086e794c46294613fbdc
SUJAY GOWDA <sujaygowda.dev@gmail.com>
2025-05-13 12:00:00 +0530

    add comments

--- event-loop.js
+++ event-loop.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 
 fs.readFile('test-file.txt', () => {
+  // 4. readFile I/O will be executed.
   console.log('I/O | File read complete');
 });
-setTimeout(() => console.log('Timer 1 finished', 0));
-setImmediate(() => console.log('Immediate 1 finished'));
-console.log('Top level code finished');
+setTimeout(() => console.log('Timer 1 finished', 0)); // 2. Timer will be executed.
+setImmediate(() => console.log('Immediate 1 finished')); // 3. setImmediate will be executed.
+console.log('Top level code finished'); // 1. Top level code will be executed.
